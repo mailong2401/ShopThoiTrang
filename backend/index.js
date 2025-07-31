fc142ef55e3b1dd279a8c96e36ce19fa6a584f7c
mailong2401 <gtmailong@gmail.com>
2025-08-01 01:03:54 +0700

    update backend and create Admin

--- backend/index.js
+++ backend/index.js
@@ -87,21 +87,24 @@ const Product = mongoose.model("Product", {
 app.post("/appproduct", async (req, res) => {
   let products = await Product.find({});
   let id;
+
   if (products.length > 0) {
-    let last_product_array = product.slice(-1);
+    let last_product_array = products.slice(-1); // ✅ dùng 'products'
     let last_product = last_product_array[0];
     id = last_product.id + 1;
   } else {
     id = 1;
   }
+
   const product = new Product({
-    id: req.body.id,
+    id: id, // ✅ tự sinh id
     name: req.body.name,
     image: req.body.image,
     category: req.body.category,
     new_price: req.body.new_price,
     old_price: req.body.old_price,
   });
+
   console.log(product);
 
   try {
@@ -112,4 +115,20 @@ app.post("/appproduct", async (req, res) => {
     console.error("Error saving product:", error.message);
     res.status(500).json({ success: false, message: "Failed to save product" });
   }
+});
+
+app.post("/removeproduct", async (req, res) => {
+  await Product.findOneAndDelete({ id: req.body.id });
+  console.log("Remove");
+  res.json({
+    success: true,
+    name: req.body.name,
+  });
+});
+
+// Creating API for getting all product
+app.get("/allproducts", async (req, res) => {
+  let products = await Product.find({});
+  console.log("All products Fetched");
+  res.send(products);
 });
