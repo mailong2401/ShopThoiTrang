cf4906e3fddb96251f32916e89c0f416d52ad20b
mailong2401 <gtmailong@gmail.com>
2025-08-06 13:48:38 +0700

    m

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,14 +1,28 @@
+
 import React from "react";
 import "./Hero.css";
 import hand_icon from "../Assets/hand_icon.png";
 import arrow_icon from "../Assets/arrow.png";
 import hero_imae from "../Assets/hero_image.png";
+import { motion } from "framer-motion";
+import "../../Pages/CSS/WetEffect.css"
 
 export const Hero = () => {
   return (
     <div className="hero">
-      <div className="hero-left">
-        <h2>CHỈ NHẬN KHÁCH ĐẾN MỚI</h2>
+      {/* Hero Left */}
+      <motion.div
+        className="hero-left"
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8 }}
+      >
+<motion.h2
+  animate={{ color: ["#ff0000", "#00ff00", "#0000ff", "#ff00ff"] }}
+  transition={{ repeat: Infinity, duration: 2 }}
+>
+  CHỈ NHẬN KHÁCH ĐẾN MỚI
+</motion.h2>
         <div>
           <div className="hero-hand-icon">
             <p>mới</p>
@@ -17,14 +31,38 @@ export const Hero = () => {
           <p>bộ sưu tầm</p>
           <p>dành cho mọi người</p>
         </div>
-        <div className="hero-latest-btn">
+
+        <motion.div
+          className="hero-latest-btn"
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+        >
           <div>Bộ sưu tập mới nhất</div>
           <img src={arrow_icon} alt="" />
-        </div>
-      </div>
-      <div className="hero-right">
-        <img src={hero_imae} alt="" />
-      </div>
+        </motion.div>
+      </motion.div>
+
+      {/* Hero Right */}
+      
+<motion.div
+  className="hero-right"
+  initial={{ opacity: 0, x: 100 }}
+  animate={{ opacity: 1, x: 0 }}
+  transition={{ duration: 0.8 }}
+  whileHover={{ scale: 1.05, rotate: 2 }}
+>
+  <motion.img
+    src={hero_imae}
+    alt=""
+    whileHover={{
+      scale: 1.1,
+      rotate: 2,
+      transition: { duration: 0.4 },
+    }}
+  />
+</motion.div>
+
     </div>
   );
 };
+
